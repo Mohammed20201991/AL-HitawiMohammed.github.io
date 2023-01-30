753e607a9fe7f00f0a0e91bc9a3f4b27bc41902d
Mohammed20201991 <mohamedabid092@gmail.com>
2023-01-30 17:46:04 +0100

    add Protofilo section

--- assets/js/main.js
+++ assets/js/main.js
@@ -86,8 +86,20 @@ modalBtns.forEach((modalBtn,i)=>{
     })
 })
 /*==================== PORTFOLIO SWIPER  ====================*/
-
-
+let swiper = new Swiper(".portfolio_container", {
+    cssMode: true,
+    loop:true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    // mousewheel: true,
+    // keyboard: true,
+  });
 /*==================== TESTIMONIAL ====================*/
 
 
